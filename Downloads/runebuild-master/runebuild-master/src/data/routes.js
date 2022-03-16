e3e06cdef95150a83cf2f6a04b4462586b8a9f8c
Jim <jiminbentley@gmail.com>
2022-03-16 13:24:22 -0400

    Inital

--- Downloads/runebuild-master/runebuild-master/src/data/routes.js
+++ Downloads/runebuild-master/runebuild-master/src/data/routes.js
@@ -4,6 +4,26 @@ const routes = [
     label: 'RuneBuild',
     path: '/',
   },
+  {
+    label: 'About',
+    path: '/about',
+  },
+  {
+    label: 'Resume',
+    path: '/resume',
+  },
+  {
+    label: 'Projects',
+    path: '/projects',
+  },
+  {
+    label: 'Stats',
+    path: '/stats',
+  },
+  {
+    label: 'Contact',
+    path: '/contact',
+  },
 ];
 
 export default routes;
