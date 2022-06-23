659e889237a2e033095b5f2961c0cc09e6f932d2
Your Name <jiminbentley@google.com>
2022-06-23 16:42:43 -0400

    changes 222

--- Desktop/leeportfolio/src/components/Template/SideBar.js
+++ Desktop/leeportfolio/src/components/Template/SideBar.js
@@ -30,7 +30,7 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Kippeum Lee <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Kippeum Lee.</p>
     </section>
   </section>
 );
